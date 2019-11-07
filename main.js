b7f26af82c02cde5991dc54df922fc7bb9960e16
邵华 <644827059@qq.com>
2019-11-07 10:11:55 +0800

    ++++

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import "babel-polyfill";
-
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, applyMiddleware, compose } from "redux";
@@ -10,16 +9,19 @@ import rootSaga from "./sagas";
 import Counter from "./Counter";
 import reducer from "./reducers";
 
+// 创建saga中间
 const sagaMiddleware = createSagaMiddleware();
-
+// 创建store
 const store = createStore(
   reducer,
+  // 启用插件
   compose(
     applyMiddleware(sagaMiddleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
+// 使用saga中的方法
 sagaMiddleware.run(rootSaga);
 
 const action = type => store.dispatch({ type });
--- sagas.js
+++ sagas.js
@@ -1,9 +1,12 @@
 import { put, takeEvery, all } from "redux-saga/effects";
 
 const delay = ms => new Promise(res => setTimeout(res, ms));
+
 // Our worker Saga: 将执行异步的 increment 任务
 export function* incrementAsync() {
+  // 延迟一秒
   yield delay(1000);
+  // 执行put方法,相当于dispatch
   yield put({ type: "INCREMENT" });
 }
 
@@ -13,9 +16,10 @@ export function* helloSaga() {
 
 // Our watcher Saga: 在每个 INCREMENT_ASYNC action spawn 一个新的 incrementAsync 任务
 export function* watchIncrementAsync() {
+  // 监听action:INCREMENT_ASYNC
   yield takeEvery("INCREMENT_ASYNC", incrementAsync);
 }
-
+// 将所有方法saga方方法导出
 export default function* rootSaga() {
   yield all([helloSaga(), watchIncrementAsync()]);
 }
